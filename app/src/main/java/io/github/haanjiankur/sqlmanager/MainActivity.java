5c61048ee3c83ad4482f902d6c0883ea37d6c299
Ankur Dhuriya <mail.ankurdhuriya@gmail.com>
2018-07-10 14:46:17 +0530

    final commit

---
+++ app/src/main/java/io/github/haanjiankur/sqlmanager/MainActivity.java
@@ -0,0 +1,42 @@
+package io.github.haanjiankur.sqlmanager;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_main );
+
+        final EditText input = (EditText)findViewById(R.id.input);
+        Button add = (Button)findViewById(R.id.add);
+        Button clear = (Button)findViewById(R.id.clear);
+        final TextView output = (TextView)findViewById(R.id.output);
+        final DBManager dbManager = new DBManager(MainActivity.this,null,null,1);
+        output.setText(dbManager.showTable().toString());
+
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dbManager.addMember(input.getText().toString());
+                output.setText(dbManager.showTable().toString());
+            }
+        });
+
+        clear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dbManager.clear(input.getText().toString());
+                output.setText(dbManager.showTable().toString());
+            }
+        });
+
+    }
+}
